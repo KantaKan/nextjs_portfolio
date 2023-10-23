a82ed569143491fb13bf98d40c8df036f19fce74
fiadaze <fiakantapon@gmail.com>
2023-10-23 07:37:39 +0700

    add link to port

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -47,7 +47,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="https://drive.google.com/file/d/12m5UusyRDBWZr0esQ96UGWN9Jnil-i84/view?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-green-400 to-teal-200 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
